8fe237ad9707550923400709b2955092f08efe41
Jesse Hietanen <jesse.hietanen@helsinki.fi>
2015-05-24 18:56:02 +0300

    Viikko2

---
+++ public_html/js/controller/tavaraController.js
@@ -0,0 +1,24 @@
+App.controller('TavaraController', function($scope){
+  $scope.tavarat = [
+        {
+            id: 1,
+            nimi: 'Rolex',
+            hinta: '55', 
+        },
+        {
+            id: 2,
+            nimi: 'Lautanen',
+            hinta: '25', 
+        }
+    ];
+    
+    
+    $scope.tav = [
+        {
+            id: 1,
+            nimi: 'Rolex',
+            hinta: '55', 
+        }];
+});
+
+
